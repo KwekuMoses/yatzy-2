1ad92fff76254594f2f080d936e961d681809fa9
GustafJ <gustaf89@hotmail.se>
2020-09-09 14:32:09 +0200

    Blablabla

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
   let sumPlayerA = document.getElementById("sum-player-a");
   let playerABonus = document.getElementById("player-a-bonus");
   countSumButton.addEventListener("click", function (e) {
-    let resultPlayerAArray = [];
     let sum = 0;
     for (let i = 0; i < 6; i++) {
       sum += Number(document.getElementById("a" + (i + 1)).value);
