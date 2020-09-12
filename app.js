06d2be3567d188c02169ad160e337686fcb4345b
Sebastian Carlson <sebcar98@gmail.com>
2020-09-12 14:13:18 +0200

    uppdaterade c

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
       //counts the sum of this players points
       for (let i = 0; i < 6; i++) {
-        let currentCell = Number(document.getElementById(this.letter + (i + 1)).value); //counts the sum of this players points
+        let currentCell = Number(
+          document.getElementById(this.letter + (i + 1)).value
+        ); //counts the sum of this players points
         sum += currentCell;
       }
 
@@ -32,9 +34,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   let wholeForm = document.getElementById("whole-form");
   let throwButton = document.getElementById("throw-dice");
 
-  throwButton.addEventListener("click", function(e) {
-    
-  });
+  throwButton.addEventListener("click", function (e) {});
 
   wholeForm.addEventListener("change", function (e) {
     playerAObject.playerSumAndBonus();
@@ -45,34 +45,34 @@ document.addEventListener("DOMContentLoaded", function (e) {
 });
 
 //skapar variablar som refererar till "tärningarna" och deras inputs"
- // Har gett dem alla samma classnamn då de sparas i en array och blir då mindre kod
- let dice = document.getElementsByClassName("diceOutput");
- let checkBox = document.getElementsByClassName("checkBox");
+// Har gett dem alla samma classnamn då de sparas i en array och blir då mindre kod
+let dice = document.getElementsByClassName("diceOutput");
+let checkBox = document.getElementsByClassName("checkBox");
 //skapar en eventlyssnare som refererar till tärningskast knappen
- document.getElementById("throw-dice").addEventListener("click", function(e){
-
- //skapar funktionen rollDice som tar dicenum.innerHTML som argument (den aktuella tärningen) och ger den ett slumpat tal 
-   function rollDice(dicenum){
-     let randomDice = Math.floor(Math.random()*(7-1)+1);  
-     dicenum.innerHTML = randomDice;  
-   };
- //Skapar funktionen IsBoxChecked som kollar ifall den aktuella checkboxen är ifylld
- //returnerar true ifall det är fallet och false vid motsatsen
- // Har gett dem alla samma classnamn då de sparas i en array och blir då mindre kod
-   function IsBoxChecked(Box){
-     if(Box.checked){
-       return true; 
-     } 
-     else{return false}
-   }
- //Skapar loop som går igenom alla checkboxar och kollar ifall dem e ifyllda eller inte, 
- //är de inte ifyllda körs funktionen rollDice
-   for(let i = 0; i < 5; ++i){
-     if(IsBoxChecked(checkBox[i]) == false){
-       rollDice(dice[i])
-   }
-   }
- });
+document.getElementById("throw-dice").addEventListener("click", function (e) {
+  //skapar funktionen rollDice som tar dicenum.innerHTML som argument (den aktuella tärningen) och ger den ett slumpat tal
+  function rollDice(dicenum) {
+    let randomDice = Math.floor(Math.random() * (7 - 1) + 1);
+    dicenum.innerHTML = randomDice;
+  }
+  //Skapar funktionen IsBoxChecked som kollar ifall den aktuella checkboxen är ifylld
+  //returnerar true ifall det är fallet och false vid motsatsen
+  // Har gett dem alla samma classnamn då de sparas i en array och blir då mindre kod
+  function IsBoxChecked(Box) {
+    if (Box.checked) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //Skapar loop som går igenom alla checkboxar och kollar ifall dem e ifyllda eller inte,
+  //är de inte ifyllda körs funktionen rollDice
+  for (let i = 0; i < 5; ++i) {
+    if (IsBoxChecked(checkBox[i]) == false) {
+      rollDice(dice[i]);
+    }
+  }
+});
 
 function isFullHouse(inputArray) {
   let diceOne = [];
